79f4923653d508c4c3966f9badf362c5c1e50636
coltonstaab1 <colton.staab@gmail.com>
2016-05-08 12:53:21 -0400

    troubleshooting with Alex

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160507180645) do
+ActiveRecord::Schema.define(version: 20160508164336) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20160507180645) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "team"
+    t.string   "gender"
+    t.integer  "age"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
