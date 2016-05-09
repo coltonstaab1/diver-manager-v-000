0325329e2343bb455579d43869fc2e31a0c2d578
coltonstaab1 <colton.staab@gmail.com>
2016-05-09 11:31:27 -0400

    Committing at end of train trip

---
+++ app/controllers/meets_controller.rb
@@ -0,0 +1,4 @@
+class MeetsController < ApplicationController
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   root :to => "static#home"
   devise_for :users, controllers: { registrations: "registrations" }
   resources :users, only: [:show, :index]
-  resources :teams
-  resources :pools
+  resources :teams, :meets, :pools
   post '/sessions/create', to: 'sessions#create'
   delete "/signout", to: "sessions#destroy"
 end
