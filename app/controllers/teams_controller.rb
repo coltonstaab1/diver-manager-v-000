345f875eb29a59a52f4629392ec5d2f8b8c745cf
coltonstaab1 <colton.staab@gmail.com>
2016-05-11 12:37:32 -0400

    Worked on new team form

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -7,5 +7,25 @@ class TeamsController < ApplicationController
   def show
     @team = Team.find(params[:id])
   end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    params
+    @team = Team.create(team_params)
+    redirect_to team_path(@team)
+  end
+
+  def edit
+    @team = Team.find(params[:id])
+    redirect_to team_path(@team)
+  end
+
+  private
+    def team_params
+      params.require(:team).permit(:name)
+    end
   
 end
