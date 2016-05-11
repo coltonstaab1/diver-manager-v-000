278c087bcc23fa8aa77bb83a0f28722b2b92601a
coltonstaab1 <colton.staab@gmail.com>
2016-05-11 17:10:58 -0400

    Created team/pool relationship when creating a new team

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,6 +3,7 @@ class Team < ActiveRecord::Base
   has_many :competition_dives, :through => :users
   has_many :dives, :through => :competition_dives
   has_one :pool
+  accepts_nested_attributes_for :pool
 
   validates :name, uniqueness: { case_sensitive: false }
 
--- db/migrate/20160511163810_add_pool_id_to_teams.rb
+++
@@ -1,5 +0,0 @@
-class AddPoolIdToTeams < ActiveRecord::Migration
-  def change
-    add_column :teams, :pool_id, :integer
-  end
-end
