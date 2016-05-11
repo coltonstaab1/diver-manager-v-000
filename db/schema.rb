3d26ac68f7140e16ea55cbe8e6d4486cca36c605
coltonstaab1 <colton.staab@gmail.com>
2016-05-11 17:01:14 -0400

    Fixed navbar issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160509152538) do
+ActiveRecord::Schema.define(version: 20160511163810) do
 
   create_table "competition_dives", force: :cascade do |t|
     t.decimal  "score_1"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20160509152538) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "pool_id"
   end
 
   create_table "users", force: :cascade do |t|
