4092a8de850b4aad41016071de494897d6aed587
coltonstaab1 <colton.staab@gmail.com>
2016-05-11 12:09:51 -0400

    Continued to modify the new meet form

--- app/controllers/meets_controller.rb
+++ app/controllers/meets_controller.rb
@@ -10,4 +10,16 @@ class MeetsController < ApplicationController
     end
   end
 
+  def create
+    binding.pry
+    params[:dives].each do |dive|
+      CompetitionDive.create(dive_params)
+    end
+  end
+
+  private
+    def dive_params(my_params)
+      my_params.permit(:score_1, :score_2, :score_3, :order_id, :user_id)
+    end
+
 end
