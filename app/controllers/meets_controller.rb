e0950e8993194435afd8aef7374e80345c0a1c0b
coltonstaab1 <colton.staab@gmail.com>
2016-05-12 18:05:21 -0400

    Troubleshooting competition dife issue

--- app/controllers/meets_controller.rb
+++ app/controllers/meets_controller.rb
@@ -3,6 +3,10 @@ class MeetsController < ApplicationController
   def index
   end
 
+  def show
+    @meet = Meet.find(params[:id])
+  end
+
   def new
     @dives = []
     6.times do 
@@ -11,15 +15,33 @@ class MeetsController < ApplicationController
   end
 
   def create
-    binding.pry
+
+    meet = Meet.find_or_create_by(meet_params(params))
     params[:dives].each do |dive|
-      CompetitionDive.create(dive_params)
+      dive = CompetitionDive.create(
+        dive_id: dive[:dive_id],
+        score_1: dive[:score_1],
+        score_2: dive[:score_2],
+        score_3: dive[:score_3],
+        order_number: dive[:order_number],
+        user_id: session[:user_id],
+        meet_id: meet.id
+        )
     end
+    binding.pry
+    redirect_to meet_path(meet)
   end
 
+
+
   private
+
+    def meet_params(my_params)
+      my_params.require(:meet).permit(:description, :pool_id)
+    end
+
     def dive_params(my_params)
-      my_params.permit(:score_1, :score_2, :score_3, :order_id, :user_id)
+      my_params.require(:dives)
     end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,9 +15,4 @@ class SessionsController < ApplicationController
     end
   end
 
-  def destroy
-    session[:user_id] = nil
-    redirect_to root_url
-  end
-
 end
